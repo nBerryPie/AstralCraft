133b69989ecdd5b3f3496ef406041d70677716a2
Nagisberry <nagisberry@gmail.com>
2016-10-29 18:37:10 +0900

    Update: ItemManager 読み込んだデータをマップに突っ込んだ

--- AstralItems/src/main/kotlin/com/nagisberry/astralitems/ItemManager.kt
+++ AstralItems/src/main/kotlin/com/nagisberry/astralitems/ItemManager.kt
@@ -5,12 +5,15 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.nagisberry.astralitems.element.Elements
+import com.nagisberry.astralitems.item.ItemData
 import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
 import java.io.FileReader
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
+import java.util.*
 
 object ItemManager {
 
@@ -18,6 +21,9 @@ object ItemManager {
 
     private val gson = Gson()
     private val logger = Bukkit.getLogger()
+
+    private val vanillaItems = HashMap<SimpleItemStack, ItemData>()
+    private val items = HashMap<String, ItemData>()
 
     fun loadFiles(dir: Path, isVanilla: Boolean = false) {
         dir.forEach { path ->
@@ -66,8 +72,11 @@ object ItemManager {
         val elements = Elements.values().map {
             json[it.name.toLowerCase()]?.asJsonObject?.let { json -> it to json }
         }.filterNotNull().map { it.first(it.second) }
-        //ToDo: ItemDataInstanceの作成
-        //ToDo: Mapに突っ込む
+        val data = ItemData(id, material, damage, name, description, rarity, types, elements)
+        if (isVanilla) {
+            vanillaItems.put(SimpleItemStack(material, damage), data)
+        } else {
+            items.put(id, data)
     }
 
     fun checkVanillaFile(path: Path) = path.fileName.toString().startsWith("vanilla", true)
