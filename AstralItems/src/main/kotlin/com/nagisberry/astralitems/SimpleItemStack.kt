a15dd75167eeee5a855e7abbbb0f83c480c7f68a
Nagisberry <nagisberry@gmail.com>
2016-10-30 11:36:31 +0900

    Update: SimpleItemStack

--- AstralItems/src/main/kotlin/com/nagisberry/astralitems/SimpleItemStack.kt
+++ AstralItems/src/main/kotlin/com/nagisberry/astralitems/SimpleItemStack.kt
@@ -2,4 +2,4 @@ package com.nagisberry.astralitems
 
 import org.bukkit.Material
 
-data class SimpleItemStack(val material: Material, val damage: Short = 0, val amount: Int = 1) {}
+data class SimpleItemStack(val material: Material, val damage: Short = 0) {}
