a75dbc38f335b0491cdcd04a7d35a7d4b4833fcd
Nagisberry <nagisberry@gmail.com>
2016-10-30 12:37:26 +0900

    Update: Elements

--- AstralItems/src/main/kotlin/com/nagisberry/astralitems/element/Elements.kt
+++ AstralItems/src/main/kotlin/com/nagisberry/astralitems/element/Elements.kt
@@ -6,5 +6,13 @@ enum class Elements(private val f: (JsonObject) -> IElement) {
 
     DAMAGEABLE({ ElementDamageable(it["base_durability"]?.asInt ?: 1) });
 
-    operator fun invoke(json: JsonObject)  = f(json)
+    operator fun invoke(json: JsonObject) = f(json)
+
+    companion object {
+        operator fun get(name: String): Elements? = try {
+            valueOf(name)
+        } catch (e: IllegalArgumentException) {
+            null
+        }
+    }
 }
