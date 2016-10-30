84e04382ea8b1ff59276fed7c2fa59071db9a914
Nagisberry <nagisberry@gmail.com>
2016-10-30 16:49:21 +0900

    Fix: dataFolderが作成されるように修正

--- AstralItems/src/main/kotlin/com/nagisberry/astralitems/AstralItems.kt
+++ AstralItems/src/main/kotlin/com/nagisberry/astralitems/AstralItems.kt
@@ -1,10 +1,14 @@
 package com.nagisberry.astralitems
 
 import org.bukkit.plugin.java.JavaPlugin
+import java.nio.file.Files
 
 class AstralItems: JavaPlugin() {
 
     override fun onEnable() {
+        if (!dataFolder.exists()) {
+            Files.createDirectory(dataFolder.toPath())
+        }
         ItemManager.loadFiles(dataFolder.toPath())
     }
 }
