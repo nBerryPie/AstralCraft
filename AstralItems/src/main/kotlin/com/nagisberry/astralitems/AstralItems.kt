3f4ed19228d9d4441a7351d898ace0d0a6fe03e6
Nagisberry <nagisberry@gmail.com>
2016-10-31 21:24:25 +0900

    Update: onEnable method

--- AstralItems/src/main/kotlin/com/nagisberry/astralitems/AstralItems.kt
+++ AstralItems/src/main/kotlin/com/nagisberry/astralitems/AstralItems.kt
@@ -1,11 +1,13 @@
 package com.nagisberry.astralitems
 
+import com.nagisberry.astralcore.command.CommandManager
 import org.bukkit.plugin.java.JavaPlugin
 import java.nio.file.Files
 
 class AstralItems: JavaPlugin() {
 
     override fun onEnable() {
+        CommandManager.registerCommand("items", this)
         if (!dataFolder.exists()) {
             Files.createDirectory(dataFolder.toPath())
         }
