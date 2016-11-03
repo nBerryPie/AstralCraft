849da10a69a642a927e0d497c9b6dbb750378d18
Nagisberry <nagisberry@gmail.com>
2016-11-03 09:26:41 +0900

    Add: onCommand method

--- AstralItems/src/main/kotlin/com/nagisberry/astralitems/AstralItems.kt
+++ AstralItems/src/main/kotlin/com/nagisberry/astralitems/AstralItems.kt
@@ -4,6 +4,9 @@ import com.google.gson.Gson
 import com.nagisberry.astralcore.command.CommandManager
 import com.nagisberry.astralitems.element.Elements
 import com.nagisberry.astralitems.item.ItemData
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 import java.io.Reader
@@ -19,7 +22,39 @@ class AstralItems: JavaPlugin() {
         ItemManager.loadFiles(dataFolder.toPath())
     }
 
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
+        if (command.name == "items") {
+            if (sender.isOp) {
+                if (args.size == 0) {
+                    sender.sendMessage("/$label [reload|get]")
+                } else if (args[0].equals("reload", true)) {
+                    ItemManager.reloadFiles(dataFolder.toPath())
+                    sender.sendMessage("[AstralItems] リロードが完了しました")
+                } else if (args[0].equals("get", true)) {
+                    if (sender !is Player) {
+                        sender.sendMessage("[AstralItems] Playerのみ実行可能なコマンドです")
+                    } else if (args.size == 1) {
+                        sender.sendMessage("/$label ${args[0]} <ItemID> (<amount>)")
+                    } else {
+                        ItemManager[args[1]]?.createItemStack(
+                                if (args.size >= 2) args[2].toInt() else 1,
+                                if (args.size >= 3) ItemManager.gson
+                                        .fromJson<Map<String, Map<String, Any>>>(
+                                                args.copyOfRange(3, args.size - 1).joinToString(" ")
+                                        )
+                                else emptyMap()
+                        )?.let {
+                            sender.inventory.addItem(it)
+                        } ?: sender.sendMessage("[AstralItems] 存在しないItemIDです")
+                    }
+                }
+                return true
+            }
+        }
+        return false
+    }
 }
+
 val ItemStack.itemData: ItemData?
     get() = ItemManager[this]
 
