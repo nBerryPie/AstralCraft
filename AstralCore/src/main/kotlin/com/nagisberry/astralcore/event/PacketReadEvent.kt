ef40b8ef956946ed0e4e2fa1c244f8e4ecaa83a1
Nagisberry <nagisberry@gmail.com>
2016-11-19 22:23:05 +0900

    Fix: typo

--- AstralCore/src/main/kotlin/com/nagisberry/astralcore/event/PacketReadEvent.kt
+++ AstralCore/src/main/kotlin/com/nagisberry/astralcore/event/PacketReadEvent.kt
@@ -4,7 +4,7 @@ import io.netty.channel.ChannelHandlerContext
 import org.bukkit.entity.Player
 import org.bukkit.event.HandlerList
 
-class PacketReadEvent(player: Player, context: ChannelHandlerContext, nessage: Any): PacketEvent(player, context, nessage) {
+class PacketReadEvent(player: Player, context: ChannelHandlerContext, message: Any): PacketEvent(player, context, message) {
 
     companion object {
         private val _handlers: HandlerList = HandlerList()
