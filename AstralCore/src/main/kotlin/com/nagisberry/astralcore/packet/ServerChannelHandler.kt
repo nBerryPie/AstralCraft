be12b69bece10365ad77296b06e9f0993d5db624
Nagisberry <nagisberry@gmail.com>
2016-11-20 10:23:48 +0900

    Update: ServerChannelHandler

--- AstralCore/src/main/kotlin/com/nagisberry/astralcore/packet/ServerChannelHandler.kt
+++ AstralCore/src/main/kotlin/com/nagisberry/astralcore/packet/ServerChannelHandler.kt
@@ -1,16 +1,31 @@
 package com.nagisberry.astralcore.packet
 
+import com.mojang.authlib.GameProfile
 import com.nagisberry.astralcore.event.StatusPacketReadEvent
 import com.nagisberry.astralcore.event.StatusPacketWriteEvent
 import io.netty.channel.ChannelDuplexHandler
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelPromise
+import net.minecraft.server.v1_10_R1.PacketLoginInStart
+import net.minecraft.server.v1_10_R1.PacketLoginOutSuccess
 import org.bukkit.Bukkit
+import kotlin.reflect.declaredMemberProperties
+import kotlin.reflect.jvm.isAccessible
 
 class ServerChannelHandler: ChannelDuplexHandler() {
 
+    private val PROPERTY_PACKET_LOGIN_OUT_SUCCESS_A = PacketLoginOutSuccess::class.declaredMemberProperties.first {
+        it.name == "a"
+    }.apply { isAccessible = true }
+
+    var profile: GameProfile? = null
+        private set
+
     override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
-        StatusPacketReadEvent(ctx, msg).let {
+        if (msg is PacketLoginInStart) {
+            profile = msg.a()
+        }
+        StatusPacketReadEvent(profile, ctx, msg).let {
             Bukkit.getServer().pluginManager.callEvent(it)
             if (!it.cancel) {
                 super.channelRead(ctx, msg)
@@ -19,7 +34,10 @@ class ServerChannelHandler: ChannelDuplexHandler() {
     }
 
     override fun write(ctx: ChannelHandlerContext, msg: Any, promise: ChannelPromise) {
-        StatusPacketWriteEvent(ctx, msg, promise).let {
+        if (msg is PacketLoginOutSuccess) {
+            profile = PROPERTY_PACKET_LOGIN_OUT_SUCCESS_A.get(msg) as GameProfile
+        }
+        StatusPacketWriteEvent(profile, ctx, msg, promise).let {
             Bukkit.getServer().pluginManager.callEvent(it)
             if(!it.cancel) {
                 super.write(ctx, msg, promise)
