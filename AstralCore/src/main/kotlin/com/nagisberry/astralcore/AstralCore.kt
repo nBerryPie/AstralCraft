a630443d328d2dd8393129e27494da313483f946
Nagisberry <nagisberry@gmail.com>
2016-11-20 10:33:22 +0900

    Update: Optimize Import

--- AstralCore/src/main/kotlin/com/nagisberry/astralcore/AstralCore.kt
+++ AstralCore/src/main/kotlin/com/nagisberry/astralcore/AstralCore.kt
@@ -5,7 +5,6 @@ import com.nagisberry.astralcore.packet.PacketManager
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 import java.io.Reader
 
